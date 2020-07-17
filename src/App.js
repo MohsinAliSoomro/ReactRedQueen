6b6c0e2c45f8165a18b5ff6230e0848d6cfeb231
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-07-17 17:01:47 +0500

    onClick queen grow and smaller

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import useWebAnimations,{lightSpeedInRight} from "@wellyshen/use-web-animations";
+import useWebAnimations from "@wellyshen/use-web-animations";
 
 function App() {
+  const { ref, animate } = useWebAnimations();
+
   
-  const { ref, playState ,getAnimation  } = useWebAnimations({...lightSpeedInRight});
+    const SmallAlice = (e) =>{
+      animate({
+        keyframes:[
+          {transform:'scale(1.1)'},
+          {transform:'scale(1.2)'},
+          {transform:'scale(1.4)'},
+        ],
+        timing:{
+          duration:3000
+        }
+      })
+     }
+     const BigAlice = (e) =>{
+      animate({
+        keyframes:[
+          {transform:'scale(1)'},
+          {transform:'scale(0)'}
+        ],
+        timing:{
+          duration:3000
+        }
+      })
+     }
+    
 
   return (
-    <div >
-      <div ref={ref} className="animate">App</div>
-      Current state: {playState}
-      <button onClick={()=>getAnimation().pause()}>Pause</button>
-      <button onClick={()=>getAnimation().play()}>Play</button>
+    <div>
+      <div ref={ref} className="alice">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/alice-hungry_colors.png" alt="alice"/>
+      </div>
+      <div className="cake">
+        <button className="btn" onClick={SmallAlice}>
+        <img src="https://2.bp.blogspot.com/-DjkZ8Og2Dck/Wj-6GFUuKDI/AAAAAAAP_FA/Y7UA2P75KOQArofFYAQhq3hLWcAxKqoQACLcBGAs/s1600/TW694690.png" alt="cake" />
+        </button>
+      </div>
+      <div className="bottle">
+        <button className="btn" onClick={BigAlice}>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/drink-me_fg_small.png" alt="setter"/>
+        </button>
+        </div>
     </div>
   );
 }
